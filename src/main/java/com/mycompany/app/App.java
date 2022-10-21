58f6411053f35966b842f4294543b7c19cdb2bc7
gogineniprathyusha <104327062+gogineniprathyusha@users.noreply.github.com>
2022-10-21 17:49:00 -0400

    test project

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! this is example test second attempt";
+    private final String message = "Hello World! this is example test third attempt";
 
     public App() {}
 
